847b9a994096e384c50c830bfdf50393f285f119
amitsengupta <amitsengupta332@gmail.com>
2025-09-15 23:59:49 +0600

    done banner video 3

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
- 
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Banner />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
