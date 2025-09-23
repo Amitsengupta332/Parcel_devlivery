84b51ff5906256d7e5f3d8d1f0563b8b0ab9e00f
amitsengupta <amitsengupta332@gmail.com>
2025-09-24 01:40:35 +0600

    auth layout created

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+export default AuthLayout
+
+//https://github.com/ProgrammingHero1/zap-shift-client-with-auth-part2/blob/main/src/layouts/AuthLayout.jsx
