8b436c342c95ad5017f183b13876aa870951c335
amitsengupta <amitsengupta332@gmail.com>
2025-09-25 01:30:46 +0600

    add registration functionality with form validation and update routing

--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,38 +1,38 @@
+import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const Register = () => {
   const {
     register,
     handleSubmit,
-
     formState: { errors },
   } = useForm();
-
   const onSubmit = (data) => {
     console.log(data);
   };
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
-        <h1 className="text-5xl font-bold">Please Login</h1>
+        <h1 className="text-5xl font-bold">Create Account</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <fieldset className="fieldset">
+            {/* email field */}
             <label className="label">Email</label>
             <input
               type="email"
-              {...register("email")}
+              {...register("email", { required: true })}
               className="input"
               placeholder="Email"
             />
+            {errors.email?.type === "required" && (
+              <p className="text-red-500">Email is required</p>
+            )}
 
             <label className="label">Password</label>
             <input
               type="password"
-              {...register("password", {
-                required: true,
-                minLength: 6,
-              })}
+              {...register("password", { required: true, minLength: 6 })}
               className="input"
               placeholder="Password"
             />
@@ -41,21 +41,22 @@ const Login = () => {
             )}
             {errors.password?.type === "minLength" && (
               <p className="text-red-500">
-                Password Must be 6 characters or longer
+                Password must be 6 characters or longer
               </p>
             )}
 
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
-
-            <button className="btn btn-primary text-black mt-4">Login</button>
+            <button className="btn btn-primary text-black mt-4">
+              Register
+            </button>
           </fieldset>
           <p>
             <small>
-              New to this website?{" "}
-              <Link className="btn btn-link" to="/register">
-                Register
+              Already have an account?{" "}
+              <Link className="btn btn-link" to="/login">
+                Login
               </Link>
             </small>
           </p>
@@ -65,4 +66,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
