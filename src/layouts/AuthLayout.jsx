846638f902ebbd012de34a680afffa02d7c903a0
amitsengupta <amitsengupta332@gmail.com>
2025-09-24 16:03:57 +0600

    add AuthLayout and Login components; update routing and package dependencies

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { Link, Outlet } from "react-router-dom";
+import authImage from "../assets/authImage.png";
+import ProFastLogo from "../pages/shared/ProFastLogo/ProFastLogo";
 
 const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
-  )
-}
+    <div className="p-12 bg-base-200">
+      <Link to="/">
+        <ProFastLogo></ProFastLogo>
+      </Link>
 
-export default AuthLayout
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="flex-1">
+          <img src={authImage} className="max-w-sm rounded-lg shadow-2xl" />
+        </div>
+        <div className="flex-1">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
 
 //https://github.com/ProgrammingHero1/zap-shift-client-with-auth-part2/blob/main/src/layouts/AuthLayout.jsx
