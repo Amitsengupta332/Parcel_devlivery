b17a67a3e9bbc674a397bda22e817ad28cbd2886
amitsengupta <amitsengupta332@gmail.com>
2025-09-25 19:38:11 +0600

    feat: implement authentication context and registration functionality

--- src/pages/Authentication/Register/Register.jsx
+++ src/pages/Authentication/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
 
 const Register = () => {
   const {
@@ -8,8 +9,17 @@ const Register = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  const { createUser } = useAuth();
   const onSubmit = (data) => {
     console.log(data);
+    createUser(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
