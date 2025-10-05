30b5c48f8488eb3dd254ceb3c74a459e957f0858
amitsengupta <amitsengupta332@gmail.com>
2025-10-05 16:41:36 +0600

    feat: integrate @tanstack/react-query for parcel management and add delete confirmation with SweetAlert2

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,21 @@ import "./index.css";
 
 import { router } from "./router/router.jsx";
 import { RouterProvider } from "react-router-dom";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import AOS from "aos";
+import "aos/dist/aos.css";
 import AuthProvider from "./contexts/AuthContext/AuthProvider.jsx";
- 
-AOS.init();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+AOS.init();
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="font-urbanist max-w-7xl mx-auto">
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
     </div>
   </StrictMode>
 );
