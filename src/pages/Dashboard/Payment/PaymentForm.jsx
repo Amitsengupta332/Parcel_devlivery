1b53e0fa25e89bda80d02463ce32c8d2bf4e762e
amitsengupta <amitsengupta332@gmail.com>
2025-10-07 00:01:17 +0600

    feat: enhance PaymentForm with error handling and styling improvements

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,9 +1,12 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-  const handleSubmit = (e) => {
+
+  const [error, setError] = useState("");
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
@@ -14,18 +17,33 @@ const PaymentForm = () => {
     if (!card) {
       return;
     }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      setError("");
+      console.log("payment method", paymentMethod);
+    }
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto">
         <CardElement className="p-2 border rounded"></CardElement>
         <button
           type="submit"
           className="btn btn-primary text-black w-full"
           disabled={!stripe}>
           {/* Pay ${amount} */}
-          Pay
+          Pay Now
         </button>
+        {error && <p className="text-red-500">{error}</p>}
       </form>
     </div>
   );
