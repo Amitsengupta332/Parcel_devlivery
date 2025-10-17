8a962a624fe58662afe527cb8e15544b029d1c39
amitsengupta <amitsengupta332@gmail.com>
2025-10-17 23:12:25 +0600

    test

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import authImage from "../assets/authImage.png";
 import ProFastLogo from "../pages/shared/ProFastLogo/ProFastLogo";
 
